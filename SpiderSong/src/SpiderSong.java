e331e337498a6fc325a9246257a0745c73a4779f
Mendelson9 <mendelson9@gmail.com>
2014-02-18 14:40:16 -0800

    Added SpiderSong project

---
+++ SpiderSong/src/SpiderSong.java
@@ -0,0 +1,12 @@
+
+public class SpiderSong {
+	static String swallowstr = "I don't know why she swallowed that fly. \n";
+	static String diestr = "Perhaps she'll die";
+	static String womanstr = "There was an old woman who slallowed a ";
+	static String catchstr = " to catch the ";
+	
+	static string firstVerse() {
+		return womanstr + ""
+	}
+
+}
